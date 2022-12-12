ea01c61028e9b833f0ed565b5314570a2c478481
qizongliang <72285030+qizongliang@users.noreply.github.com>
2022-12-11 23:34:07 -0500

    Added new seed code and adjust the campus and student model

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -3,25 +3,31 @@
 
 It defines the campus model for the database.
 ==================================================*/
-const Sequelize = require('sequelize');  // Import Sequelize
-const db = require('../db');  // Import Sequelize database instance called "db"
+const Sequelize = require('sequelize') // Import Sequelize
+const db = require('../db') // Import Sequelize database instance called "db"
 
 // Define the campus model
-const Campus = db.define("campus", {
+const Campus = db.define('campus', {
   name: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
 
   address: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
-
   description: {
     type: Sequelize.STRING,
-  }
-});
+    allowNull: true,
+  },
+  imageURL: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    defaultValue:
+      'https://qns.com/wp-content/uploads/2021/11/QC-Rosenthal-Library.png',
+  },
+})
 
 // Export the campus model
-module.exports = Campus;
+module.exports = Campus
